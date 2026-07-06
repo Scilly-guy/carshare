cf831ae3000c0cba13a699ad3e5d124363ee2749
Scilly-guy <harry.legg86@googlemail.com>
2026-07-06 14:48:35 +0100

    use relative path to fetch available vehicles

--- theme/static/handle-available-vehicles.js
+++ theme/static/handle-available-vehicles.js
@@ -1,6 +1,4 @@
-const base_url=document.getElementById("base_url").getAttribute("href");
 const available_vehicles_path=document.getElementById("available_vehicles_url").getAttribute("href");
-const available_vehicles_url=new URL(available_vehicles_path,window.location.origin);
 const csrftoken=document.querySelector("[name=csrfmiddlewaretoken]").value;
 const start_input=document.getElementById("id_reservation_time_0");
 const end_input=document.getElementById("id_reservation_time_1");
@@ -59,7 +57,7 @@ const available_vehicles={
 }
 
 function get_available_vehicles(){
-    fetch(available_vehicles_url,
+    fetch(available_vehicles_path,
         {
             method:"POST",
             headers:{"X-CSRFToken": csrftoken },
